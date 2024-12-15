0bf8422a3befbcc2c640ca117fcabe4547b68f58
GGBongDJB <18301786727@qq.com>
2024-12-15 12:23:16 +0800

    feature: 提交 MySQL 数据库分页插件

--- project/src/main/java/com/nageoffer/shortlink/project/config/DataBaseConfiguration.java
+++ project/src/main/java/com/nageoffer/shortlink/project/config/DataBaseConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class DataBaseConfiguration {
 
     /**
-     * 分页插件
+     * MySQL分页插件
      */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
